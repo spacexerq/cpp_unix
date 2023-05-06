54cff4fd1a90e740928d3dff86055277732a8c80
spacexerq <bns2002@yandex.ru>
2023-05-06 06:32:55 +0300

    script create

---
+++ dev_to_stg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+timetag=$(date '+%d.%m.%Y.%H.%M.%S')
+git checkout stg 
+git merge --commit dev
+git tag "$timetag"
+git push origin "$timetag"
+git checkout dev
+git commit -m "revision transfered from dev to stg"
