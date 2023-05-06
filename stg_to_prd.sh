ad00c5c362f6b4e3a39079b40a17c8f2dac8a498
spacexerq <bns2002@yandex.ru>
2023-05-06 06:33:54 +0300

    script create

---
+++ stg_to_prd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+timetag=$(date '+%d.%m.%Y.%H.%M.%S')
+git checkout prd
+git merge --commit stg
+git tag "$timetag"
+git push origin "$timetag"
+git checkout dev
+git commit -m "revision transfered from stg to prd"
